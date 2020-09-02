764bfc18de092449b2b07916e8c9be0f6bb7a699
David Svetlecic <DSvetlecic@cylance.com>
2020-09-02 17:07:58 -0500

    Show Remaining Todos

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -3,12 +3,17 @@ describe('List item', () => {
     cy.seedAndVisit()
   })
 
-  it.only('display completed items properly', () => {
+  it('display completed items properly', () => {
     cy.get('.todo-list li')
       .filter('.completed')
       .should('have.length', 1)
       .and('contain', 'Eggs')
       .find('.toggle')
       .should('be.checked')
+  })
+
+  it.only('show remaining todos count in the footer', () => {
+    cy.get('.todo-count')
+      .should('contain', 3)
   })
 })
